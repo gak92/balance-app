1f05bf63a481e33c2a2a763686bb70ecd174a4b1
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-06 19:14:51 +0500

    Add navbar and routes

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <button className='btn btn-primary mx-2'>-</button>
+      Increase or decrease Balance
+      <button className='btn btn-primary mx-2'>+</button>
+    </div>
+  )
+}
+
+export default Home;
